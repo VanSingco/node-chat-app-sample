21f523876bbf75ea0c463d7b172a0556ee9566b3
Van Zachary Singco19 <edwincarneses@gmail.com>
2018-06-16 07:27:28 +0800

    developing chat app

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,43 +10,24 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(msg){
     const formattedTime = moment(msg.createdAt).format('h:mm a');
-    console.log('Got new message', msg)
-    $('#messages').append(
-        `<li class="chat-room_chat--user-chat-1">
-            <div class="d-flex chat-room__chat--user-chat-1__user">
-                <span data-toggle="tooltip" data-placement="top" title="${msg.from}">
-                    <img src="./assets/img/profile-default.jpg" class="m-2" alt="">
-                </span>
-                <div class="mt-2">
-                     <div class="chat-text d-flex align-items-center align-self-center">
-                         <p>${msg.text}</p>   
-                     </div>
-                    <small class="mt-2 float-right mr-3">${formattedTime}</small>
-                </div>
-            </div>
-        </li>`
-    )
+    const template = $('#message-template').html();
+    const html = Mustache.render(template, {
+        from: msg.from,
+        text: msg.text,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(msg){
     const formattedTime = moment(msg.createdAt).format('h:mm a');
-    $('#messages').append(
-        `<li class="chat-room_chat--user-chat-1">
-            <div class="d-flex chat-room__chat--user-chat-1__user">
-                <span data-toggle="tooltip" data-placement="top" title="${msg.from}">
-                    <img src="./assets/img/profile-default.jpg" class="m-2" alt="">
-                </span>
-                <div class="mt-2">
-                    <div class="chat-text d-flex align-items-center align-self-center">
-                        <a target="_blank" href="${msg.url}">
-                            <p>my current location</p>
-                        </a>   
-                     </div>
-                    <small class="mt-2 float-right mr-3">${formattedTime}</small>
-                </div>
-            </div>
-        </li>`
-    )
+    const template = $('#messageLocation-template').html();
+    const html = Mustache.render(template, {
+        from: msg.from,
+        url: msg.url,
+        createdAt: formattedTime
+    })
+    $('#messages').append(html)
 })
 
 socket.emit('createMessage',{
